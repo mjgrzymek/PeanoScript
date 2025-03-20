b57c72ad6149113d5cd896d779d1558ee2308439
Michal Grzymek <mjgrzymek@gmail.com>
2025-03-20 23:04:01 +0100

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         <Navbar />
         <main className="mx-auto max-w-7xl px-1 py-6 lg:px-8">{children}</main>
+        <Analytics />
       </body>
     </html>
   );
